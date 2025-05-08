7ea195c8fc3217c49be91e2aea5b80f0abef8c3e
Prashant Verma <prashantverma.vn@gmail.com>
2025-05-08 12:14:24 +0530

    correction in db file

--- database.js
+++ database.js
@@ -1,10 +1,11 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
+import { uri } from "./resources";
 
 // Replace the placeholder with your Atlas connection string
-const uri = "mongodb+srv://prashantverma:Proxx%401998@first-database.bvss8dy.mongodb.net/";
+const url = uri
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri,  {
+const client = new MongoClient(url,  {
         serverApi: {
             version: ServerApiVersion.v1,
             strict: true,
